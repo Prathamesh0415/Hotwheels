69fe651daa0c6d30df171194fd23ae6f9923a394
Prathamesh Ghormare <prathameshghormare@gmail.com>
2025-04-12 09:40:45 +0530

    some more fixes on page 5

--- src/Pages/Page5.jsx
+++ src/Pages/Page5.jsx
@@ -9,7 +9,7 @@ function Page5() {
       </h1>
 
       {/* Center red car - always visible */}
-      <div className="w-60 sm:w-[374.03px] h-28 sm:h-[186.01px] absolute top-[150px] sm:top-[200px] left-1/2 transform -translate-x-1/2 rotate-[20.08deg]">
+      <div className="w-60 sm:w-[374.03px] h-28 sm:h-[186.01px] absolute top-[220px] sm:top-[200px] left-1/2 transform -translate-x-1/2 rotate-[20.08deg]">
         <img
           src="../assets/car54.png"
           alt="Red racing car"
@@ -18,7 +18,7 @@ function Page5() {
       </div>
 
       {/* Orange car bottom center - always visible */}
-      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 w-60 sm:w-[348.63px] h-40 sm:h-[261.47px] rotate-[-20.08deg]">
+      <div className="absolute bottom-[60px] sm:bottom-10 left-1/2 transform -translate-x-1/2 w-60 sm:w-[348.63px] h-40 sm:h-[261.47px] rotate-[-20.08deg]">
         <img
           src="../assets/car51.png"
           alt="Orange muscle car"
