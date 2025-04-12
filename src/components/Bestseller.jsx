4e0fc123cb754895b10572bae9eb6ff708834903
Prathamesh Ghormare <prathameshghormare@gmail.com>
2025-04-12 09:28:53 +0530

    implement minor fixes

--- src/components/Bestseller.jsx
+++ src/components/Bestseller.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 function Bestseller() {
   return (
     <div
-      className="w-full h-24 flex items-center justify-center px-8 "
+      className="w-full h-24 sm:h-32 flex items-center justify-center px-4 sm:px-8"
       style={{
         backgroundImage: "url('/assets/Bestseller.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-        <div className="text-white font-extrabold text-7xl luckiest-guy-regular">
-            BESTSELLERS
-        </div>
+      <div className="text-white font-extrabold text-4xl sm:text-6xl md:text-7xl text-center luckiest-guy-regular">
+        BESTSELLERS
+      </div>
     </div>
   )
 }
